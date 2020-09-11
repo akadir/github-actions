7ee0ea1b9c488557573d6b4ee1fc2c2e947cbbb6
kadir <akadirkkoc@gmail.com>
2020-09-11 22:18:22 +0300

    chore: trigger github actions

--- src/main/java/io/github/akadir/github/actions/GithubActions.java
+++ src/main/java/io/github/akadir/github/actions/GithubActions.java
@@ -4,6 +4,6 @@ import io.github.akadir.github.actions.foo.Calculate;
 
 public class GithubActions {
     public static void main(String[] args) {
-        System.out.println("Hello world: " + Calculate.add(2, 3));
+        System.out.println("Hello world. " + Calculate.add(2, 3));
     }
 }
